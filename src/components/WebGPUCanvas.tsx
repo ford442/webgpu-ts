2e947a5b6d19229dd194e4a06a768ac53411566b
Noah Cohn <noahc42@gmail.com>
2025-08-27 02:00:48 -0700

    Update WebGPUCanvas.tsx

--- src/components/WebGPUCanvas.tsx
+++ src/components/WebGPUCanvas.tsx
@@ -25,7 +25,7 @@ const WebGPUCanvas: React.FC<WebGPUCanvasProps> = ({ mode, zoom, panX, panY }) =
                 videoRef.current.src = 'https://www.w3schools.com/html/mov_bbb.mp4';
                 videoRef.current.muted = true;
                 videoRef.current.loop = true;
-                videoRef.current.crossorigin = true;
+                videoRef.current.crossOrigin = true;
                 videoRef.current.autoplay = true;
                 videoRef.current.playsInline = true;
                 videoRef.current.play();
