f1d6bef507a0a80330157b8eac80eb34251ac648
Noah Cohn <noahc42@gmail.com>
2025-09-14 15:49:59 -0700

    Update Renderer.ts

--- src/renderer/Renderer.ts
+++ src/renderer/Renderer.ts
@@ -55,16 +55,16 @@ private MAX_FIRE_POINTS = 50;
             format: this.presentationFormat,
             alphaMode: 'premultiplied',
         });
+
+        await this.fetchImageUrls(); // Fetch the list from our Google Bucket
+        await this.createResources();
+        await this.createPipelines();
 this.fireBindGroup = this.device.createBindGroup({
     layout: this.firePipeline.getBindGroupLayout(0),
     entries: [
         { binding: 0, resource: { buffer: this.fireUniformBuffer } },
     ],
 });
-        await this.fetchImageUrls(); // Fetch the list from our Google Bucket
-        await this.createResources();
-        await this.createPipelines();
-
         return true;
     }
 
