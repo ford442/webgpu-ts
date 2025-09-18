f9fcb6edfcee92953a8d63338c356c46f23e18a2
Noah Cohn <noahc42@gmail.com>
2025-09-18 01:48:03 -0700

    Update WebGPUCanvas.tsx

--- src/components/WebGPUCanvas.tsx
+++ src/components/WebGPUCanvas.tsx
@@ -87,7 +87,7 @@ const WebGPUCanvas: React.FC<WebGPUCanvasProps> = ({ mode, zoom, panX, panY, ima
     };
 
     return (
-        <canvas ref={canvasRef} width="800" height="600" onMouseMove={handleCanvasMouseMove} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseLeave={handleMouseLeave} />
+        <canvas ref={canvasRef} width="2048" height="2048" onMouseMove={handleCanvasMouseMove} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseLeave={handleMouseLeave} />
     );
 };
 
