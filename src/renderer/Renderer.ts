7be638efc6be9fbc06db2266d5b5584368bc4a97
Noah Cohn <noahc42@gmail.com>
2025-09-18 00:29:00 -0700

    Update Renderer.ts

--- src/renderer/Renderer.ts
+++ src/renderer/Renderer.ts
@@ -126,7 +126,15 @@ export class Renderer {
 
         this.bindGroups.set('image', this.device.createBindGroup({ layout: this.pipelines.get('imageVideo')!.getBindGroupLayout(0), entries: [{ binding: 0, resource: this.sampler }, { binding: 1, resource: this.imageTexture.createView() }, { binding: 2, resource: { buffer: this.imageVideoUniformBuffer } }] }));
         this.bindGroups.set('liquid', this.device.createBindGroup({ layout: this.pipelines.get('liquid')!.getBindGroupLayout(0), entries: [{ binding: 0, resource: this.sampler }, { binding: 1, resource: this.writeTexture.createView() }] }));
-        this.bindGroups.set('computeV1', this.device.createBindGroup({ layout: this.pipelines.get('computeV1')!.getBindGroupLayout(0), entries: [{ binding: 0, resource: this.sampler }, { binding: 1, resource: this.imageTexture.createView() }, { binding: 2, resource: this.writeTexture.createView() }, { binding: 3, resource: { buffer: this.v1ComputeUniformBuffer } }] }));
+  this.bindGroups.set('computeV1', this.device.createBindGroup({ 
+            layout: this.pipelines.get('computeV1')!.getBindGroupLayout(0), 
+            entries: [
+                { binding: 0, resource: this.sampler }, 
+                { binding: 1, resource: this.imageTexture.createView() }, 
+                { binding: 2, resource: this.writeTexture.createView() },
+                // { binding: 3, resource: { buffer: this.v1ComputeUniformBuffer } } // This line was incorrect and has been removed.
+            ] 
+        }));
         this.bindGroups.set('compute', this.device.createBindGroup({ layout: this.pipelines.get('compute')!.getBindGroupLayout(0), entries: [{ binding: 0, resource: this.sampler }, { binding: 1, resource: this.imageTexture.createView() }, { binding: 2, resource: this.writeTexture.createView() }, { binding: 3, resource: { buffer: this.v2ComputeUniformBuffer } }] }));
     }
 
