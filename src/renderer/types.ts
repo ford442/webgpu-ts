8f3c272925bd6fbfa24dae20fd589c76c823a3e6
Noah Cohn <noahc42@gmail.com>
2025-09-22 22:02:01 -0700

    Update types.ts

--- src/renderer/types.ts
+++ src/renderer/types.ts
@@ -1,3 +1,2 @@
-// src/renderer/types.ts
-
+// This file is included for completeness, but no changes were needed here.
 export type RenderMode = 'shader' | 'image' | 'video' | 'ripple' | 'liquid-v1' | 'liquid' | 'liquid-v3' | 'depth';
