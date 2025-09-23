6fee8672154bca07d0d52d4daa913379e6ef4c13
Noah Cohn <noahc42@gmail.com>
2025-09-22 18:49:28 -0700

    Update types.ts

--- src/renderer/types.ts
+++ src/renderer/types.ts
@@ -1,1 +1,3 @@
-export type RenderMode = 'shader' | 'image' | 'video' | 'ripple' | 'liquid-v1' | 'liquid' | 'liquid-v3';
+// src/renderer/types.ts
+
+export type RenderMode = 'shader' | 'image' | 'video' | 'ripple' | 'liquid-v1' | 'liquid' | 'liquid-v3' | 'depth';
