8ca4739f0c42095cea8e40efff586b18bf459e8b
Noah Cohn <noahc42@gmail.com>
2025-10-08 04:18:01 -0700

    Update types.ts

--- src/renderer/types.ts
+++ src/renderer/types.ts
@@ -1,11 +1,2 @@
 export type RenderMode = 
-  'shader' | 
-  'image' | 
-  'video' | 
-  'ripple' | 
-  'liquid-v1' | 
-  'liquid' | 
-  'liquid-zoom' | 
-  'liquid-vortex' |
-  'liquid-perspective' |
-  'vortex'; // ADD THIS
+  '3d-zoom';
