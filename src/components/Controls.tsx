0e76f366654210fdf84f550e0347fb09e7fa6905
Noah Cohn <noahc42@gmail.com>
2025-10-09 04:09:35 -0700

    Update Controls.tsx

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -8,16 +8,9 @@ interface ControlsProps {
     onLoadModel: () => void;
     isModelLoaded: boolean;
     isRendererReady: boolean;
-
-    // Props for '3d-zoom'
-    depthThreshold: number; setDepthThreshold: (v: number) => void;
-    edgeHardness: number; setEdgeHardness: (v: number) => void;
-    depthLevels: number; setDepthLevels: (v: number) => void;
-    fogColor: string; setFogColor: (v: string) => void;
-    fogDensity: number; setFogDensity: (v: number) => void;
+    // For 3D Zoom
     parallaxStrength: number; setParallaxStrength: (v: number) => void;
-    
-    // Props for '3d-parallax'
+    // For 3D Parallax
     displacementScale: number; setDisplacementScale: (v: number) => void;
     ambientLight: number; setAmbientLight: (v: number) => void;
     smoothness: number; setSmoothness: (v: number) => void;
@@ -44,32 +37,12 @@ const Controls: React.FC<ControlsProps> = (props) => {
             </div>
 
             {/* --- Sliders for 3D Zoom --- */}
-            {props.mode === '3d-zoom' && (<>
-                <div className="control-group">
-                    <label htmlFor="depth-slider">Depth Cutoff:</label>
-                    <input type="range" id="depth-slider" min="0" max="100" value={props.depthThreshold * 100} onChange={(e) => props.setDepthThreshold(parseFloat(e.target.value) / 100)} />
-                </div>
-                <div className="control-group">
-                    <label htmlFor="hardness-slider">Edge Hardness:</label>
-                    <input type="range" id="hardness-slider" min="1" max="100" value={props.edgeHardness * 100} onChange={(e) => props.setEdgeHardness(parseFloat(e.target.value) / 100)} />
-                </div>
-                <div className="control-group">
-                    <label htmlFor="levels-slider">Depth Levels:</label>
-                    <input type="range" id="levels-slider" min="2" max="16" step="1" value={props.depthLevels} onChange={(e) => props.setDepthLevels(Number(e.target.value))} />
-                </div>
-                <div className="control-group">
-                    <label htmlFor="fog-color">Fog Color:</label>
-                    <input type="color" id="fog-color" value={props.fogColor} onChange={(e) => props.setFogColor(e.target.value)} />
-                </div>
-                <div className="control-group">
-                    <label htmlFor="fog-density">Fog Density:</label>
-                    <input type="range" id="fog-density" min="0" max="100" value={props.fogDensity * 10} onChange={(e) => props.setFogDensity(parseFloat(e.target.value) / 10)} />
-                </div>
+            {props.mode === '3d-zoom' && (
                 <div className="control-group">
                     <label htmlFor="parallax-strength">Parallax Strength:</label>
                     <input type="range" id="parallax-strength" min="0" max="100" value={props.parallaxStrength * 1000} onChange={(e) => props.setParallaxStrength(parseFloat(e.target.value) / 1000)} />
                 </div>
-            </>)}
+            )}
 
             {/* --- Sliders for 3D Parallax --- */}
             {props.mode === '3d-parallax' && (<>
