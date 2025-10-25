e1572b6d5b3a4a307584772ae996c428c1d8d67e
Noah Cohn <noahc42@gmail.com>
2025-10-25 01:32:32 -0700

    Update Renderer.ts

--- src/renderer/Renderer.ts
+++ src/renderer/Renderer.ts
@@ -66,7 +66,7 @@ export class Renderer {
         });
         this.context = this.canvas.getContext('webgpu')!;
         this.presentationFormat = navigator.gpu.getPreferredCanvasFormat();
-        this.context.configure({device: this.device, format: this.presentationFormat, alphaMode: 'premultiplied'});
+        this.context.configure({device: this.device, colorSpace: "display-p3",format: this.presentationFormat, alphaMode: 'premultiplied',toneMapping: {mode: "extended"}});
         await this.fetchImageUrls();
         await this.createResources();
         await this.createPipelines();
