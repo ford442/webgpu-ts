cb8903629f2e95b7c7d7fe000247dceac34887e8
Noah Cohn <noahc42@gmail.com>
2025-11-17 10:07:04 -0800

    Update Renderer.ts

--- src/renderer/Renderer.ts
+++ src/renderer/Renderer.ts
@@ -449,7 +449,7 @@ export class Renderer {
         this.filteringSampler = this.device.createSampler({
             magFilter: 'linear',
             minFilter: 'linear',
-            mipmapFilter" 'linear',
+            mipmapFilter: 'linear',
             addressModeU: 'repeat',
             addressModeV: 'repeat',
             maxAnisotropy: 16,
